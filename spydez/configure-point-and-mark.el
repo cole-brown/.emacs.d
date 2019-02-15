a6dceee6171ea649af2e5910687bfa1bb4037441
Cole Brown <git@spydez.com>
2019-02-15 10:06:27 -0800

    Added 'pager.el' package.
    It's orphaned, but I like it too much to abandon it too.

--- spydez/configure-point-and-mark.el
+++ spydez/configure-point-and-mark.el
@@ -49,6 +49,26 @@ point reaches the beginning or end of the buffer, stop there."
 
 
 ;;------------------------------------------------------------------------------
+;; Pager
+;;------------------------------------------------------------------------------
+;; Better paging up/down in emacs buffer. E.g. Page dn + Page up = exactly
+;; where your point used to be (down to col,row).
+;;
+;; Orphaned: but I like it, so hopefully the orphanage keeps it around.
+;; https://github.com/emacsorphanage/pager
+(use-package pager
+  :bind
+  (("C-v" . pager-page-down)
+   ([next] . pager-page-down)
+   ("M-v" . pager-page-up)
+   ([prior] . pager-page-up)
+   ([M-up] . pager-row-up)
+   ([M-kp-8] . pager-row-up)
+   ([M-down] . pager-row-down)
+   ([M-kp-2] . pager-row-down)))
+
+
+;;------------------------------------------------------------------------------
 ;; Multiple Cursors
 ;;------------------------------------------------------------------------------
 ;; TODO: test these out?
