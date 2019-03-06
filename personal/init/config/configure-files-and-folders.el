e9b68d94ebcf80dce879967e70124a20ff9f3181
Cole Brown <git@spydez.com>
2019-03-05 18:07:42 -0800

    Added treemacs. Found bug in treemacs/no-littering.
    Put notes about the bug in docs/errors. Should really put an issue
    or pull request on no-littering and/or treemacs, maybe?

--- personal/init/config/configure-files-and-folders.el
+++ personal/init/config/configure-files-and-folders.el
@@ -184,6 +184,59 @@ Version 2017-09-01"
 
 
 ;;------------------------------------------------------------------------------
+;; Treemacs
+;;------------------------------(that name tho)---------------------------------
+;; https://github.com/Alexander-Miller/treemacs
+(use-package treemacs
+  :defer t
+
+
+  ;;:bind
+  ;; bind some keys so you can get into treemacs?
+
+
+  :config
+  ;; Annoying error from (maybe?) no-littering/treemacs interactions...
+  ;;   condition-case: Opening output file: No such file or directory,
+  ;;   <path-to>/.emacs.d/.cache/treemacs-persist-at-last-error
+  ;;
+  ;; Treemacs wants a file at .cache/treemacs-persist-at-last-error
+  ;; and there is not one so kaboom? Or is it the directory?
+  ;;
+  ;; Not sure that's the intended functionality for the very first call of
+  ;; something treemacs related... /s
+  ;; Can we move it over to no-littering next to the other one?
+  ;; in no-littering.el: (setq treemacs-persist-file (var "treemacs-persist.org"))
+  (setq treemacs--last-error-persist-file (no-littering-expand-var-file-name
+                                           "treemacs-persist-at-last-error.org"))
+  ;; Yeah, that fixed it... Probably should try to throw that up at
+  ;; no-littering in a pull request or something. Or treemacs. *shrug*
+  ;; both maybe.
+
+  ;; treemacs-persist-at-last-error.org now has this for contents:
+  ;; # State when last error occured on 2019-03-05 17:45:25
+  ;; # Error was 'Cannot end with a project or workspace name' in line '* Default Workspace'
+  ;;
+  ;; * Default Workspace
+
+  ;; many settings that can be set can be set here
+
+  ;; The default width and height of the icons is 22 pixels. If you are
+  ;; using a Hi-DPI display, uncomment this to double the icon size.
+  ;;(treemacs-resize-icons 44)
+  )
+
+(use-package treemacs-projectile
+  :after treemacs projectile)
+
+(use-package treemacs-icons-dired
+  :after treemacs dired
+  :config (treemacs-icons-dired-mode))
+
+(use-package treemacs-magit
+  :after treemacs magit)
+
+;;------------------------------------------------------------------------------
 ;; TODO: these. Parenthesis, bells?
 ;;------------------------------------------------------------------------------
 ;; here or configure-text? text right now.
