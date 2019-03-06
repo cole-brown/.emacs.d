b0be53cb96cce128784564382be1327de509d5d2
Cole Brown <git@spydez.com>
2019-03-06 12:37:22 -0800

    Added epa/gpg issue. Added prereqs to other issues' org files.
    Moved epa-gpg notes/complaints/curse words/etc into org file and cleaned up code.

--- personal/init/config/configure-crypt.el
+++ personal/init/config/configure-crypt.el
@@ -10,139 +10,14 @@
 ;; https://serverfault.com/questions/86048/gpg-what-do-i-need-to-backup
 ;;   - so... zip up that gpg dir if you think any magic blue smoke is about to escape?
 
-;;------------------------------------------------------------------------------
-;; General Cursing and Gnashing of Teeth.
-;;------------------------------------------------------------------------------
-
-
-;; TODO: we expect GnuPG to exist, so make sure spydez/tools/external exists
-;; and has "gpg" in it. Or do I already have a func for tool-exists?
-;; Well it's worse than that and yes I do.
-
-
-;; Followed this for making key, checking that it exists in both GPG
-;; and M-x epa-list-secret-keys.
-;;   https://www.masteringemacs.org/article/keeping-secrets-in-emacs-gnupg-auth-sources
-
-
-;; Initial error:
-;;   GnuPG: “decryption failed: secret key not available”
-;;
-;; NOTE: gpg version should be identical? Created a key on (a Git Bash) command line with
-;;   > gpg --version
-;;   gpg (GnuPG) 2.2.9-unknown
-;;   libgcrypt 1.8.3
-;; Then in emacs I tried to do stuff, and it encrypted it, but cannot decrypt it now?
-;;   emacs
-;;   M-x shell
-;;   > gpg --version
-;;   gpg (GnuPG) 1.4.21
-;;
-;; So... Got to find the uppest to datest GPG in emacs.
-;;   https://stackoverflow.com/a/7319251
-
-;; Ok. Had to chunk some old GitHub app junk in the Windows PATH, and then also
-;; update my bootstrap-this-late.el to point to correct place (mingw
-;; installation that's part of Git for Windows)
-;;
-;; Might should consider prepending external tools to paths instead of postpending.
-;;
-;; Now we have the correct version. We still can't do anything. :(
-
-
-;; Error now:
-;;   GPG error: "no usable configuration", OpenPGP
-
-;; So... we're not working still. Internet says we need this:
-;;   (custom-set-variables '(epg-gpg-program  "/usr/local/bin/gpg2"))
-;;
-;; Or maybe this:
-;;   (custom-set-variables
-;;    '(epg-gpg-home-directory "c:/Users/MYUSER/AppData/Roaming/gnupg")
-;;    '(epg-gpg-program "C:/Progra~2/gnupg/bin/gpg.exe")
-;;    '(epg-gpgconf-program "c:/progra~2/gnupg/bin/gpgconf.exe")
-;;   )
-;;  https://emacs.stackexchange.com/questions/21699/how-to-configure-easypg-with-gpg4win-or-any-other-windows-gpg-version
-
-;; So here. Does this work?
-;;   (require 'epa-file)
-;;   (custom-set-variables
-;;    '(epg-gpg-home-directory (spydez/dir-name ".gnupg" (getenv "HOME")))
-;;    )
-;; No. Error now:
-;;   GPG error: "no usable configuration", OpenPGP
-
-;; Or this?!
-;;   (require 'epa-file)
-;;   (custom-set-variables
-;;    '(epg-gpg-home-directory (spydez/dir-name ".gnupg" (getenv "HOME")))
-;;    '(epg-gpg-program (executable-find "gpg"))
-;;    '(epg-gpgconf-program (executable-find "gpgconf"))
-;;    )
-;; ...Maybe? Probably not?
-;; emacs:
-;;   M-x epa-list-secret-keys
-;;   > just an empty buffer
-;; bash:
-;;   gpg -k (or gpg -K)
-;;   > info about my gpg key.
-
-;; So, no. Still doesn't work. Try to open my test encrypted gpg file and get:
-;;   Error while decrypting with "c:/Program Files/Git/usr/bin/gpg.exe":
-;;   gpg: Fatal: can't create directory '/c/home/cole/.emacs.d/spydez/c:/home/cole/.gnupg': No such file or directory
-
-;; So............ We're in some unholy limbo of windows and linux... -_-
-;;
-;; gpgconf's output is linuxy, in Git Bash anyways. That may be throwing it off the scent.
-;; Options maybe?:
-;;   1) Install a GnuPG that's for windows and don't rely on Git for Windows to provide it.
-;;   2) Bash on ahead like all we have is a hammer.
-
-;; So, dumb idea:
-;;   (require 'epa-file)
-;;   (custom-set-variables
-;;    '(epg-gpg-home-directory "/c/home/cole/.gnupg/") ;; nixy path instead of windowsy
-;;    '(epg-gpg-program (executable-find "gpg"))
-;;    '(epg-gpgconf-program (executable-find "gpgconf"))
-;;    )
-
-;; Dumb idea may have worked.
-;; emacs:
-;;   M-x epa-list-secret-keys
-;;   > info about my gpg key.
-;; bash:
-;;   gpg -k (or gpg -K)
-;;   > info about my gpg key.
-
-;; Can I simplify down to just the home dir? That'll make it slightly less of a
-;; complete abomination.
-;;   (require 'epa-file)
-;;   (custom-set-variables
-;;    '(epg-gpg-home-directory "/c/home/cole/.gnupg/") ;; nixy path instead of windowsy
-;;    )
-
-;; emacs:
-;;   M-x epa-list-secret-keys
-;;   GPG error: "no usable configuration", OpenPGP
-;; Nope. Gotta be NC-17 full frontal abomination.
-
-;; Second computer:
-;;   Was silently failing...
-;;   Had to get epg-gpg-home-directory happy with its unixy-on-windows
-;;   abomination of a path. M-x epa-list-keys and M-x epa-list-secret-keys both
-;;   were just empty buffer until I uglied enough hacks together to get that
-;;   variable kludged into shape.
-
 
 ;;------------------------------------------------------------------------------
-;;                         General Middle Fingers to:
-;;
-;;   EPA, Emacs, GPG, Bash, Git, Windows, Git for Windows, Bash for Git for
-;;                 Windows, GPG for Bash for Git for Windows,
-;;      and optimism (I had so much I thought "one more quick feature")
+;; EasyPG, GPG, Etc.
 ;;------------------------------------------------------------------------------
+;; So much went wrong setting this up... see `spydez/dir/docs/issues' for
+;; notes and solution.
 
-;; So this one finally works. I can see my key; I can decrypt/encrypt my test file.
+;; This finally works. I can see my key; I can decrypt/encrypt my test file.
 ;; TODO: use vars and set 'em in an overridable manner.
 ;; TODO: figure out how to integrate this into use-tool
 (require 'epa-file)
@@ -151,35 +26,14 @@
 ;; So just muck it on our own and we're like a hacky onion. In muck.
 (let ((spydez/hack/ugly-hacky-gpg-dir
        (concat (spydez/dir/windows-to-mingw spydez/dir/home) ".gnupg/")))
-  ;; (setq epg-gpg-home-directory spydez/hack/ugly-hacky-gpg-dir) ; unixy path instead of windowsy
-  ;; (setq epg-gpg-program (executable-find "gpg"))               ; windowsy
-  ;; (setq epg-gpgconf-program (executable-find "gpgconf"))       ; windowsy
-
-  ;; Well... I can set them here, but they'll keep wanting to be in custom.el
-  ;; If I do. And I don't really want them in custom.el... I was hoping for that
-  ;; to be a global file. But setq doesn't work for these for some reason.
-  ;; So this is a fun pickle.
-
-  ;; This also fucks up my intentions for use-tool quite badly... :/
-  ;; Can't move configuration like this into use-tool if its forced
-  ;; to exist in the custom file.
-
   ;; TODO: what the fuck to do with these... I don't really want these in
   ;; custom file...
-  ;; Try this csetq macro for setting? https://oremacs.com/2015/01/17/setting-up-ediff/
-  ;; Same result in this case. Settings wander into custom file if it is saved by something else.
   (custom-set-variables
    '(epg-gpg-home-directory spydez/hack/ugly-hacky-gpg-dir) ; unixy path instead of windowsy
    '(epg-gpg-program (executable-find "gpg"))               ; windowsy
    '(epg-gpgconf-program (executable-find "gpgconf"))       ; windowsy
    ))
 ;; TODO: get gpg more cross-computery via use-tool
-
-;; Don'th think I need this:
-;;;;(epa-file-enable)
-;; Possibly don't need (require 'epa-file) either.
-
-;; TODO: move rant into use-tool, hopefully, when vars are moved/integrated there too.
 
 ;; https://zzamboni.org/post/my-emacs-configuration-with-commentary/
 
