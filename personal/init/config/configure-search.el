fd4121b5f286a813e95cc940ceafa9fd1c5e43c5
Cole Brown <git@spydez.com>
2019-03-09 16:50:34 -0800

    Unborrowing some more code.

--- personal/init/config/configure-search.el
+++ personal/init/config/configure-search.el
@@ -5,6 +5,11 @@
 ;; General Settings
 ;;------------------------------------------------------------------------------
 
+;; If emacs grep or <package>-grep are looking in dirs you don't want, try
+;; adding to this list: `grep-find-ignored-directories'
+;; e.g. from http://nhoffman.github.io/.emacs.d/#org9119f86
+;;   - Add ".eggs" and "src" to list for python development.
+
 
 ;;------------------------------------------------------------------------------
 ;; Blank
@@ -15,35 +20,9 @@
 ;; Python
 ;;------------------------------------------------------------------------------
 
-;;   "Native emacs grep functions (like M-x rgrep) as well as projectile-grep
-;; and helm-projectile-grep all ignore directories specified by the variable
-;; grep-find-ignored-directories. Let's add some to the defaults."
-;;   - http://nhoffman.github.io/.emacs.d/#org9119f86
-;; Why would he not want to grep source directory?..
-;; (when (boundp 'grep-find-ignored-directories)
-;;   (add-to-list 'grep-find-ignored-directories ".eggs")
-;;   (add-to-list 'grep-find-ignored-directories "src"))
-
-;;   "Make a function to ignore contents of a project's virtualenv, and advise
-;; functions using grep to apply it before execution."
-;;   - http://nhoffman.github.io/.emacs.d/#org9119f86
-;; I'm not using python or virtual env so avoiding for now...
-;; (defun grep-ignore-venv-current-project (&rest args)
-;;   (interactive)
-;;   (let ((venv (find-venv-current-project)))
-;;     (if venv
-;;         (progn
-;;           (setq venv (file-name-nondirectory
-;;                       (replace-regexp-in-string "/$" "" venv)))
-;;           (message "adding '%s' to grep-find-ignored-directories" venv)
-;;           (add-to-list 'grep-find-ignored-directories venv))
-;;       (message "no virtualenv at this location")
-;;       )))
-;;
-;; (advice-add 'rgrep :before #'grep-ignore-venv-current-project)
-;; (advice-add 'projectile-grep :before #'grep-ignore-venv-current-project)
-;; (advice-add 'helm-projectile-grep :before #'grep-ignore-venv-current-project)
-
+;; If using venv (virtual env), try using advice-add to dynamically put the
+;; project's venv dir into the grep ignored list.
+;; (Have to add it to each grep function: rgrep, <package>-grep...)
 
 
 ;;------------------------------------------------------------------------------
