5bd77e4e70fe10b122f6916de91877040f7601c3
Cole Brown <git@spydez.com>
2019-03-16 14:59:15 -0700

    Updates to home domain to get working dev-directories.
    Home still is behind by a hunspell external tool.

--- personal/dev/computers/plan9-2418-d188/bootstrap-this-early.el
+++ personal/dev/computers/plan9-2418-d188/bootstrap-this-early.el
@@ -20,31 +20,8 @@
   ;; Directories
   ;;---
 
-  ;; todo: move these dirs to secrets?
+  ;; TODO: get rid of this step if it's just empty for all computers?
 
-  ;; todo: Not sure here or dropbox...
-  (defconst spydez/dir/common-doc-save "d:/vault/documents/emacs"
-    "Place for auto-open files or secrets or something to be.")
-
-  (defconst spydez/dir/dropbox "d:/Users/spydez/Dropbox"
-    "Dropbox's location on local disk...")
-
-  ;; todo: move this list to secrets?
-  ;; auto-open this file list at end of emacs init/setup
-  ;; setq to just override the whole thing
-  (defconst spydez/auto-open-list
-    ;; don't do: '(
-    ;; do this: (list
-    ;; Need a thing that will eval the list items to strings either now or in auto-open-files.
-    ;; So we choose now.
-    (list
-     ;; top level work org-mode file for notes & such
-     (expand-file-name "42.org" (spydez/dir-name "42" spydez/dir/dropbox))
-
-     ;; Working on .emacs.d a lot right now so add these in.
-     ;; (expand-file-name "configure-completion.el" spydez/dir/emacs/personal)
-     (expand-file-name "init.el" spydez/dir/emacs)
-     ))
 
   ;;------------------------------------------------------------------------------
   ;; TODOs
---
+++ personal/dev/domains/home/dev-directories.el
@@ -0,0 +1,76 @@
+;; -*- mode: emacs-lisp; lexical-binding: t -*-
+
+
+;;------------------------------------------------------------------------------
+;; General Settings
+;;------------------------------------------------------------------------------
+
+
+;;------------------------------------------------------------------------------
+;; Constants
+;;------------------------------------------------------------------------------
+
+(defconst spydez/dir/common-doc-save "d:/vault/documents/42"
+  "Place for auto-open files or secrets or something to be.")
+
+(defconst spydez/dir/dropbox "d:/Users/spydez/Dropbox"
+  "Dropbox's location on local disk...")
+
+;; Stuff I don't quite want in my public repo will be in these somewhere...
+
+(defconst spydez/dir/secrets (spydez/dir-name ".secrets.d" spydez/dir/home)
+  "Location of secrets dir on this computer.")
+(defconst spydez/file/secrets (expand-file-name "emacs.secrets.el.gpg" spydez/dir/secrets)
+  "Location of emacs' elisp secrets.")
+
+(defconst spydez/dir/secrets/dev
+  (spydez/dir-name "dev" spydez/dir/secrets)
+  "Dev config outside of .emacs.d.")
+
+(defconst spydez/dir/secrets/dev/defaults
+  (spydez/dir-name "defaults" spydez/dir/secrets/dev)
+  "All of my optional/default setup elisp files...")
+
+(defconst spydez/dir/secrets/dev/domain-all
+  (spydez/dir-name "domains" spydez/dir/secrets/dev)
+  "Domains folder. For subdirs of work, home, etc.")
+
+(defconst spydez/dir/secrets/dev/domain-this
+  (spydez/dir-name spydez/setup/domain/name spydez/dir/secrets/dev/domain-all)
+  "Anything that has to be domain specific. Tab widths or whatnot.")
+
+(defconst spydez/dir/secrets/dev/system-all
+  (spydez/dir-name "computers" spydez/dir/secrets/dev)
+  "Computers folder. For subdirs of different computers.")
+
+(defconst spydez/dir/secrets/dev/system-this
+  (spydez/dir-name spydez/setup/system/hash spydez/dir/secrets/dev/system-all)
+  "Anything that has to be computer specific. Overriding tab widths or whatnot.")
+
+
+;;------------------------------------------------------------------------------
+;; Path
+;;------------------------------------------------------------------------------
+
+;; Now we can push even more dirs onto the load path.
+;; Also this may be getting a skosh out of hand...
+
+;; NOTE: these are all higher priority than any .emacs.d path, right now,
+;; so take care.
+;; Leaving Off: spydez/dir/secrets/dev
+(add-to-list 'load-path spydez/dir/secrets/dev/defaults)
+(add-to-list 'load-path spydez/dir/secrets/dev/domain-all)
+(add-to-list 'load-path spydez/dir/secrets/dev/domain-this)
+(add-to-list 'load-path spydez/dir/secrets/dev/system-all)
+(add-to-list 'load-path spydez/dir/secrets/dev/system-this) ;; most specific to this computer last
+
+
+;;------------------------------------------------------------------------------
+;; TODOs
+;;------------------------------------------------------------------------------
+
+
+;;------------------------------------------------------------------------------
+;; The End.
+;;------------------------------------------------------------------------------
+(provide 'dev-directories)
