9da64ebfae7c60908637e2f64b87806f1d7b79c1
Cole Brown <git@spydez.com>
2019-05-02 12:47:40 -0700

    2 todos into working-on. Taskspace finished split-name func.

--- personal/lisp/taskspace.el
+++ personal/lisp/taskspace.el
@@ -23,17 +23,35 @@
 ;;   - <date>: yyyy-mm-dd format
 ;;   - <#>:    two digit number starting at zero, for multiple tasks per day
 ;;   - <description>: don't care - human info
-(defconst spydez/tasks/separator/dir-name "_"
+
+(defconst spydez/tasks/dir-name/separator "_"
   "Split directory name on this to extract date, dedup number, and description.")
+
+(defconst spydez/tasks/dir-name/parts-alists
+  '(
+    ;; Three part. Code does this all the time?
+    ((date . 0)
+     (number . 1)
+     (description . 2))
+    ;; Two part. Human does this most of the time...
+    ((date . 0)
+     (description . 2))
+   )
+  "Order of items in task's descriptive directory name. List of alists.
+First one of the correct length is used currently.")
+;; (cdr (assoc 'date (nth 0 spydez/tasks/dir-name/parts-alists)))
 
 
 ;;------------------------------------------------------------------------------
 ;; Taskspace Commands
 ;;------------------------------------------------------------------------------
 
-;; TODO: open spydez/dir/tasks in dired mode buffer
-
+;; TODO: command to copy full task's dir path to clipboard
+;;   - will need the dwim today's task, I think?
+;;   - could just stub out dwim with "find today's pre-existing else taskspace root dir"
 ;; TODO: dwim today's task ? (create if none, choose from multiple, open the one...)
+
+;; TODO: open spydez/dir/tasks in dired mode buffer
 
 ;; TODO: dwim <date>'s task ?
 
@@ -45,22 +63,35 @@
 ;; Taskspace Utils
 ;;------------------------------------------------------------------------------
 
-;; TODO: util to split up dir name and then give bits back?
+;; util to split up dir name and then give desired bit back
 ;;  - should work for manually made ones that don't have the middle <#> part
 (defun spydez/taskspace/split-name (name part)
   "Splits name based on taskspace naming/separator rules and returns the
 requested part. Part can be one of: '(date number description)."
-  (let ((split-name (split-string name spydez/tasks/separator/dir-name)))
-    (when (not (null split-name))
-      ;; now cond? cond instead of when?
+  (unless (null name)
+    ;; unless or if/error?
+    (let* ((split-name (split-string name spydez/tasks/dir-name/separator))
+           (len-split (length split-name))
+           (split-alist))
 
-  ;; TODO-TODAY: hi
+      ;; find the right alist for parsing the split dir string
+      (dolist (alist spydez/tasks/dir-name/parts-alists split-alist)
+        (when (= len-split (length alist))
+          (setq split-alist alist)
+          ))
+
+      ;; figure out what index is desired,
+      ;; then pull out the desired string (and return it)
+      (nth (cdr (assoc part split-alist)) split-name)
       )))
+;; (spydez/taskspace/split-name "2000_0_foo" 'date)
 
-(defun spydez/taskspace/dir= (dir name part)
-  ;; todo-today: strip dir down file name
-  ;; strip file name down to part
-  ;; check against name?
+(defun spydez/taskspace/dir= (name dir part)
+  "True if `name' is equal to the split-name `part' of `dir'.
+Else nil."
+  ;; todo-today: strip dir down to file name
+  ;; strip file name down to part (if non-nil part)
+  ;; check against input name
   )
 
 
@@ -92,7 +123,7 @@ requested part. Part can be one of: '(date number description)."
         (date-dirs)) ;; return val
     (dolist (dir task-dirs date-dirs)
       (let ((split (split-string (file-name-nondirectory dir)
-                                 spydez/tasks/separator/dir-name)))
+                                 spydez/tasks/dir-name/separator)))
         (when (and (not (null split))
                    (string= date-str (car split)))
           ;; This dir's first bit is equal to today's formatted date.
