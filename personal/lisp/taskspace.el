d6b78f766cc9e5a306b67b16bef5cd44ca9261ff
Cole Brown <git@spydez.com>
2019-05-02 18:48:07 -0700

    Taskspace work. Start of task-name/dwim.
    Started dwim func for getting dir of today's task.
    
    Updated dir= to use split-name func.

--- personal/lisp/taskspace.el
+++ personal/lisp/taskspace.el
@@ -49,11 +49,40 @@ First one of the correct length is used currently.")
 ;; TODO: command to copy full task's dir path to clipboard
 ;;   - will need the dwim today's task, I think?
 ;;   - could just stub out dwim with "find today's pre-existing else taskspace root dir"
-;; TODO: dwim today's task ? (create if none, choose from multiple, open the one...)
+
+;; TODO: dwim today's task ? (create if none, choose from multiple, return if just the one...)
+(defun spydez/taskspace/task-name/dwim (arg)
+  "DWIM to return today's task dir string (full path)...
+Create if none. Return if just the one. Choose from multiple."
+  (interactive "p") ;; numeric prefix arg
+  ;; format-time-string: use optional TIME arg for "not-today" (see help)
+  (let* ((date (format-time-string spydez/datetime/format/yyyy-mm-dd))
+         (taskspaces (spydez/taskspace/list-date date)))
+
+    (cond ((null date) (error "Date string is nil: %s" date))
+
+          ;; TODO: if zero, create one (prompt for short desc)
+          ;;  - just alphanumeric and space/-, convert spaces to dashes?
+          ((null taskspaces) (error "TODO: No taskspace for date %s. Create one here." date))
+
+          ;; If just one, return it.
+          ;; How to create second in that case? Use a non-dwim create func?
+          ;;   - I think yes.
+          ((= 1 (length taskspaces)) (car taskspaces))
+
+          ;; TODO: if 2+, prompt for which to return (ideally by fuzzy match?)
+          ((> 1 (length taskspaces))
+           (error "TODO: Multiple taskspaces. Prompt for which to return (ideally by fuzzy match?)"))
+
+          ;; Don't need a default case... Fall through with nil.
+          ;;(t nil)
+    )))
+;; M-x spydez/taskspace/task-name/dwim
+;; (spydez/taskspace/task-name/dwim 0)
 
 ;; TODO: open spydez/dir/tasks in dired mode buffer
 
-;; TODO: dwim <date>'s task ?
+;; TODO: dwim <date>'s task ? (is this a dupe of spydez/taskspace/task-name/dwim?)
 
 ;; TODO: optional/prefix arg for yesterday, day before, etc? Or use org-mode's
 ;; calendar picker thing?
@@ -68,7 +97,7 @@ First one of the correct length is used currently.")
 (defun spydez/taskspace/split-name (name part)
   "Splits name based on taskspace naming/separator rules and returns the
 requested part. Part can be one of: '(date number description)."
-  (unless (null name)
+  (unless (or (null name) (null part))
     ;; unless or if/error?
     (let* ((split-name (split-string name spydez/tasks/dir-name/separator))
            (len-split (length split-name))
@@ -85,14 +114,25 @@ requested part. Part can be one of: '(date number description)."
       (nth (cdr (assoc part split-alist)) split-name)
       )))
 ;; (spydez/taskspace/split-name "2000_0_foo" 'date)
+;; (spydez/taskspace/split-name "2000_0_foo" nil)
+
 
 (defun spydez/taskspace/dir= (name dir part)
   "True if `name' is equal to the split-name `part' of `dir'.
 Else nil."
-  ;; todo-today: strip dir down to file name
-  ;; strip file name down to part (if non-nil part)
-  ;; check against input name
-  )
+  ;; don't accept nulls
+  (unless (or (null name) (null dir) (null part))
+    ;; strip dir down to file name and
+    ;; strip file name down to part (if non-nil part)
+    (let* ((dir-name (file-name-nondirectory dir))
+           (dir-part (spydez/taskspace/split-name dir-name part)))
+      (if (null dir-part)
+          nil ;; don't accept nulls
+        ;; else, usable data
+        ;; check against input name
+        (string= name dir-part)
+        ))))
+;; (spydez/taskspace/dir= "2000" "c:/foo/bar/2000_0_testcase" 'date)
 
 
 ;; Get children directories of spydez/dir/tasks, ignoring
@@ -115,21 +155,16 @@ Else nil."
 ;; (message "%s" (spydez/taskspace/list-all))
 
 
-;; Get all, pare list down to today, return.
-;;   - split on "_", then string= on today's date's string.
+;; Get all, pare list down to date-str, return.
 (defun spydez/taskspace/list-date (date-str)
   "Get any/all taskspaces for today."
-  (let ((task-dirs (spydez/taskspace/list-all))
-        (date-dirs)) ;; return val
-    (dolist (dir task-dirs date-dirs)
-      (let ((split (split-string (file-name-nondirectory dir)
-                                 spydez/tasks/dir-name/separator)))
-        (when (and (not (null split))
-                   (string= date-str (car split)))
-          ;; This dir's first bit is equal to today's formatted date.
-          ;; Yay.
+  (unless (null date-str)
+    (let ((task-dirs (spydez/taskspace/list-all))
+          (date-dirs)) ;; return val
+      (dolist (dir task-dirs date-dirs)
+        (when (spydez/taskspace/dir= date-str dir 'date)
           (push dir date-dirs)
-      )))))
+          )))))
 ;; (spydez/taskspace/list-date "2019-04-25")
 
 ;; TODO: if 0 today, prompt user for short description in minibuffer,
