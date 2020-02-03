af356244a7452f1d556c9ca9eee3daaa29f60aef
Cole Brown <code@brown.dev>
2020-02-03 11:28:55 -0800

    Added smarter beginning and end of visual line funcs.
    `spydez/smarter-beginning-of-visual-line' replaces `beginning-of-visual-line'
    with functionality similar to `spydez/smarter-move-beginning-of-line'. Point
    will now move to start of visual line, then call
    `spydez/smarter-move-beginning-of-line' for more options if that did nothing.
    
    So "C-a" in e.g. org-mode will now move to:
      1) beginning of visual line
      2) beginning of logical line
      3) indentation of logical line
    
    Similarly for "C-e", new `spydez/smarter-end-of-visual-line' replaces
    `end-of-visual-line'. This will move point to:
      1) end of visual line
      2) end of logical line

--- personal/init/config/configure-point-and-mark.el
+++ personal/init/config/configure-point-and-mark.el
@@ -43,12 +43,16 @@ Result depends on syntax table's comment character."
 ;;------------------------------------------------------------------------------
 ;; What is "the 'beginning' of the 'line'" anyways?
 
+;;---
+;; Logical Lines
+;;---
+
 ;; https://emacsredux.com/blog/2013/05/22/smarter-navigation-to-the-beginning-of-a-line/
-;; Trial [2019-01-29]
-;; Trial 2 ([2019-05-17]): Do beginning of line first, not second?
-;;   This still could be annoying in, for example, macros... We'll see.
+;; Trial 1 [2019-01-29]: Use code from link.
+;; Trial 2 [2019-05-17]: Do beginning of line first, not second.
+;; TRIAL END [2020-02-03]: Trial successful; keep this.
 (defun spydez/smarter-move-beginning-of-line (arg)
-  "Move point to beginning of line.
+  "Move point to beginning of line, or indentation.
 
 Move point to the beginning of the line. If point is already
 there, move to the first non-whitespace character on this line.
@@ -56,7 +60,8 @@ Effectively toggle between the beginning of the line and the
 first non-whitespace character.
 
 If ARG is not nil or 1, move forward ARG - 1 lines first.  If
-point reaches the beginning or end of the buffer, stop there."
+point reaches the beginning or end of the buffer, stop there.
+"
   (interactive "^p")
   (setq arg (or arg 1))
 
@@ -65,20 +70,96 @@ point reaches the beginning or end of the buffer, stop there."
     (let ((line-move-visual nil))
       (forward-line (1- arg))))
 
+  ;; Wasn't liking C-a having unexpected effects. Was originally:
+  ;;   1) Go to first non-whitespace.
+  ;;   2) Else go to beginning of line.
+  ;; I've reversed those so one C-a works how my fingers expect but spamming
+  ;; is useful to remind me of the new functionality.
   (let ((orig-point (point)))
     (move-beginning-of-line 1)
+    ;; If that did nothing, jump to indentation.
     (when (= orig-point (point))
       (back-to-indentation))))
+
 
 ;; remap C-a to `smarter-move-beginning-of-line'
 (bind-key [remap move-beginning-of-line]
           'spydez/smarter-move-beginning-of-line)
 
-;; Wasn't liking C-a having unexpected effects. Was originally:
-;;   1) Go to first non-whitespace.
-;;   2) Else go to beginning of line.
-;; I've reversed those so one C-a works how my fingers expect but spamming
-;; is useful to remind me of the new functionality.
+
+;;---
+;; Visual Lines
+;;---
+
+(defun spydez/smarter-beginning-of-visual-line (arg)
+  "Move point to beginning of visual line, or actual line, or indentation.
+
+Move point to the beginning of the (visual) line. If point is
+already there, move point to the beginning of the (actual/logical) line.
+If point is already there, move to the first non-whitespace
+character on this line. Effectively toggle between the beginning
+of the visual line, logical line, and the first non-whitespace
+character.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there.
+"
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  ;; Move in the line now.
+  (let ((orig-point (point)))
+    (beginning-of-visual-line 1)
+    ;; If that did nothing, jump into `spydez/smarter-move-beginning-of-line'
+    ;; for more beginnings.
+    (when (= orig-point (point))
+      (spydez/smarter-move-beginning-of-line 1))))
+
+
+(defun spydez/smarter-end-of-visual-line (arg)
+  "Move point to end of visual line, or actual line.
+
+Move point to the end of the (visual) line. If point is already
+there, move point to the end of the (actual/logical) line.
+Effectively toggle between the end of the visual line and
+logical line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there.
+"
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  ;; Move in the line now.
+  (let ((orig-point (point)))
+    (end-of-visual-line 1)
+    ;; If that did nothing, jump to end of actual/logical line.
+    (when (= orig-point (point))
+      (move-end-of-line 1))))
+
+
+;; remap C-a to `smarter-beginning-of-visual-line' in visual-line-mode-map
+(bind-keys :map visual-line-mode-map
+           ;; beginning of line
+           ([remap beginning-of-visual-line]
+            . spydez/smarter-beginning-of-visual-line)
+           ([remap move-beginning-of-line]
+            . spydez/smarter-beginning-of-visual-line)
+           ;; end of line
+           ([remap end-of-visual-line]
+            . spydez/smarter-end-of-visual-line)
+           ([remap move-end-of-line]
+            . spydez/smarter-end-of-visual-line))
 
 
 ;;------------------------------------------------------------------------------
