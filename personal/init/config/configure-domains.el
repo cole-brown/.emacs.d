74bbf73e7b7d9a858faf6bac1935c458989b73d7
Cole Brown <git@spydez.com>
2020-07-21 15:59:30 -0700

    Added `spydez/dirky/dired' interactive function.
    Function will take you to the directory described by keybase/dirky.

--- personal/init/config/configure-domains.el
+++ personal/init/config/configure-domains.el
@@ -67,6 +67,75 @@ Can just define as (ignore) or something.
                domain-key))))
 ;; (spydez/domains/include :jeff)
 
+
+;;------------------------------------------------------------------------------
+;; Interactives
+;;------------------------------------------------------------------------------
+
+(defvar spydez/dirky/dired/history nil
+   "Var to hold separate history for `spydez/dirky/dired' command.")
+
+(defun spydez/dirky/dired ()
+  "Visit a dirky directory via dired.
+"
+  (interactive)
+  (let* ((keybase
+          ;; Keybase get.
+          (intern
+           (completing-read
+            "Keybase:"
+
+            ;; Keybase can be any of these keys ("columns") from the alist:
+            ;; ...which we will complicate a bit so that some popular ones are
+            ;; up top.
+            (-distinct
+             (-flatten
+              (list
+               ;; Some things to get preferred up towards the top...
+               (-select-column 0 spydez/dev/domains)
+               :default
+               :secrets
+
+               ;; And this actually has all the keys
+               (-select-column 0 spydez/dirky/keys))))
+            ;; Already did our list filter shenanigans, thanks.
+            nil
+
+            ;; Make 'em confirm if going off the menu.
+            'confirm
+
+            nil
+            nil
+            ;; Do I want a default value?
+            spydez/dirky/domain)))
+
+         ;; Dirkey get.
+         (dirkey
+          (intern
+           (completing-read
+            "Dir Key:"
+
+            ;; Keybase can be any of these keys ("columns") from the
+            ;; alist of our keybase:
+            (-select-column 0 (alist-get keybase
+                                         spydez/dirky/keys))
+            nil
+
+            ;; Make 'em confirm if going off the menu.
+            'confirm
+
+            nil
+            ;; History for this arg of this command.
+            'spydez/dirky/dired/history)))
+
+         ;; Might as well build the path here too.
+         (path (spydez/dirky/path keybase dirkey)))
+
+    (message "Dirky<%S %S>: %S"
+             keybase dirkey path)
+    (dired path)))
+
+
 ;;------------------------------------------------------------------------------
 ;; The End.
 ;;------------------------------------------------------------------------------
